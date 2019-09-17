0d6bd7a28d4495a3cfcbd504b2e498478f0396b8
Andre Alves Garzia <andre@andregarzia.com>
2019-09-18 00:29:17 +0100

    fix deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,10 @@
 #lang info
 (define collection "web-view")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define deps '("gui-lib"
+               "base"))
+(define build-deps '("at-exp-lib"
+                     "gui-doc"
+                     "scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/web-view.scrbl" ())))
 (define pkg-desc "A web-view control to be used with Racket GUI toolkit applications")
 (define version "0.1")
